7da21571201cd5a1555268cdd90e3ba5adff1bb6
Swapnabir Dutta <accibc@gmail.com>
2024-09-11 15:12:51 +0530

    updated index.js file for databse url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = "mongodb+srv://accibc:passmongo@todomini.lavqw.mongodb.net/?retryWrites=true&w=majority&appName=toDomini";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
